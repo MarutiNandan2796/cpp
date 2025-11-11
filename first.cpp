43cef9994532aef243b16ae9cae1be2b3f51b508
Maruti Nandan <marutinandan885817@gmail.com>
2025-11-11 12:52:13 +0530

    intial change

--- first.cpp
+++ first.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 using namespace std;
-int product(int a,int b=6){
-    int prod=a*b;
-    cout<<prod;
-    return 0;
-}
 int main(){
-    product(5);
-    return 0;
-
+    // int n;
+    // cin>>n;
+    int arr[5]={100,120,10,45,5};
+    // for(int i=0;i<5;i++){
+    //     cin>>arr[i];
+    // }
+    int max=arr[0];
+    for(int i=0;i<5;i++){
+        if(arr[i]>max){
+            max=arr[i];
+        }
+    }
+    cout<<max<<endl;
 }
